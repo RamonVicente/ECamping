c2611e149e33445f8bf8bcf29b5ce97898ab9c1e
Ramon Vicente <ramonvicentee@gmail.com>
2018-07-14 09:24:31 -0300

    Adição de perfil de campista

--- src/main/java/com/ecamping/beans/ListaCampistaBean.java
+++ src/main/java/com/ecamping/beans/ListaCampistaBean.java
@@ -10,10 +10,12 @@ import com.ecamping.entidade.User;
 import com.ecamping.service.CampingService;
 import com.ecamping.service.UserService;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 /**
@@ -21,14 +23,19 @@ import javax.faces.context.FacesContext;
  * @author ramon
  */
 @ManagedBean(name = "listaCampistas")
-@RequestScoped
+@SessionScoped
 public class ListaCampistaBean {
     
     private List<User> campistas;
     private User campista;
-
+    private User campistaAtual;
     @EJB
     UserService userService;
+    
+    @PostConstruct
+    public void iniciar() {
+        campistaAtual  = (User) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("campistaAtual");
+    }
 
     public List<User> getCampistas() {
         campistas = userService.getAllUsers();
@@ -41,17 +48,45 @@ public class ListaCampistaBean {
     }
     
     public String redirectToProfile(User campista){
-        this.campista = campista;
-        return "perfilCampista";
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("campistaAtual", campista);
+        return "perfilCampista?faces-redirect=true";
     }
     
     public void editProfile(){
-        this.userService.update(campista);
+        this.userService.update(this.campistaAtual);
+        addMessage("Campista editado com sucesso!");
     }
+    
     
     public void addMessage(String summary) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
+
+    public User getCampista() {
+        return campista;
+    }
+
+    public void setCampista(User campista) {
+        this.campista = campista;
+    }
+
+    public User getCampistaAtual() {
+        return campistaAtual;
+    }
+
+    public void setCampistaAtual(User campistaAtual) {
+        this.campistaAtual = campistaAtual;
+    }
+
+    public UserService getUserService() {
+        return userService;
+    }
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+    
+    
     
 }
